0b7683d2c848a19aac0fd11a8eba38fd7b7ece9e
Dominic LoBue <dominic.lobue@gmail.com>
2015-05-13 14:12:32 -0700

    add metadata

---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             "solo_client"
+maintainer       "Dominic LoBue"
+maintainer_email "dominic.lobue@gmail.com"
+license          "Apache 2.0"
+description      "apt cookbook"
+long_description IO.read(File.join(File.dirname(__FILE__), 'README'))
+version          "0.0.1"
+
